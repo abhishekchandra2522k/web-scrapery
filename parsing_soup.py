012189b7eab9acdd3f42c687baa34a911e6c18ed
abhishekchandra2522k <1ac23456789@gmail.com>
2020-12-23 22:30:13 +0530

    Modify parsing_soup.py

--- parsing_soup.py
+++ parsing_soup.py
@@ -13,6 +13,14 @@ results = soup.find("ol", {"id":"b_results"})
 
 links = results.findAll("li",{"class":"b_algo"})
 
-for items in links:
+for item in links:
     item_text = item.find("a").text
-    item_href = item.find()
+    item_href = item.find("a").attrs["href"]
+
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)
+        #print("Summary:", item.find("a").parent.parent.find("p").text)
+        children = item.children
+        for child in children:
+            print("Child",child)
